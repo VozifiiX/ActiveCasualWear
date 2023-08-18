9c592db200eb440f14c3cba5d1b06181bdc74540
VozifiiX <123703026+VozifiiX@users.noreply.github.com>
2023-08-17 21:50:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,23 @@
-/* Reset default margin and padding */
-body, h1, h2, h3, p, ul, li {
-    margin: 0;
-    padding: 0;
+let cartItems = [];
+
+function addToCart(productName) {
+    cartItems.push(productName);
+    updateCartContent();
 }
 
-/* Basic styling for header and navigation */
-header {
-    background-color: #333;
-    color: #fff;
-    text-align: center;
-    padding: 1rem;
+function updateCartContent() {
+    const cartSection = document.getElementById('cart');
+    const cartList = cartSection.querySelector('.cart-list');
+
+    if (cartItems.length === 0) {
+        cartList.innerHTML = '<p>Your cart is currently empty.</p>';
+    } else {
+        const itemsHTML = cartItems.map(item => `<li>${item}</li>`).join('');
+        cartList.innerHTML = `<ul>${itemsHTML}</ul>`;
+    }
 }
 
-nav ul {
-    list-style: none;
-}
-
-nav ul li {
-    display: inline;
-    margin-right: 20px;
-}
-
-nav ul li a {
-    color: #ff8800; /* Orange color */
-    text-decoration: none;
-    font-weight: bold;
-}
-
-/* Styling for sections */
-section {
-    padding: 2rem;
-}
-
-section h2 {
-    font-size: 2rem;
-    color: #333;
-    margin-bottom: 1rem;
-}
-
-section p {
-    font-size: 1.2rem;
-    color: #777;
-}
-
-/* Styling for buttons */
-.btn {
-    display: inline-block;
-    padding: 10px 20px;
-    background-color: #ff8800; /* Orange color */
-    color: #fff;
-    text-decoration: none;
-    margin-top: 10px;
-    border-radius: 5px;
-    cursor: pointer;
-}
-
-.btn-google {
-    background-color: #dc4e41; /* Google Red */
-}
-
-.email-signup {
-    margin-top: 20px;
-    padding: 20px;
-    background-color: #f5f5f5;
-    border-radius: 5px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-/* Styling for footer */
-footer {
-    background-color: #333;
-    color: #fff;
-    text-align: center;
-    padding: 1rem;
+function signInWithGoogle() {
+    // Your Google sign-in implementation here
 }
 
