248bf76feb95dced2bb908670cff80c277ee81f8
VozifiiX <123703026+VozifiiX@users.noreply.github.com>
2023-08-17 20:09:45 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buyButtons = document.querySelectorAll('.buy-button');
-    buyButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const productName = this.getAttribute('data-product');
-            alert(`Thank you for purchasing ${productName}!`);
-        });
-    });
-});
